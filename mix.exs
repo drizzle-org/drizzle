265f56100f53f9301053102c8d0e0669182d12ca
Jon Carstens <jjcarstens@me.com>
2020-06-04 12:41:28 -0600

    Move mix.exs dependency management to utilize Mix.target() organization

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule Drizzle.MixProject do
 
   @app :drizzle
   @target System.get_env("MIX_TARGET") || :host
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
 
   def project do
     [
@@ -11,9 +12,7 @@ defmodule Drizzle.MixProject do
       elixir: "~> 1.9",
       target: @target,
       archives: [nerves_bootstrap: "~> 1.6"],
-      deps_path: "deps/#{@target}",
-      build_path: "_build/#{@target}",
-      lockfile: "mix.lock.#{@target}",
+      build_embedded: true,
       start_permanent: Mix.env() == :prod,
       aliases: [loadconfig: [&bootstrap/1]],
       deps: deps(),
@@ -61,31 +60,25 @@ defmodule Drizzle.MixProject do
       {:nerves, "~> 1.6", runtime: false},
       {:shoehorn, "~> 0.6"},
       {:toolshed, "~> 0.2"},
-    ] ++ deps(@target)
-  end
-
-  # Specify target specific dependencies
-  defp deps(:host), do: []
-
-  defp deps(target) do
-    [
-      {:nerves_time, "~> 0.2"},
-      {:nerves_pack, "~> 0.3.0"},
-      {:nerves_firmware_ssh, "~> 0.3"},
-      {:nerves_runtime_shell, "~> 0.1.0"},
-      {:ring_logger, "~> 0.8.0"},
       {:vintage_net, "~> 0.7.9"},
-      {:vintage_net_wizard, "~> 0.2.3"}
-    ] ++ system(target)
-  end
+      {:vintage_net_wizard, "~> 0.2.3"},
 
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.8", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.8", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.8", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.8", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 2.3", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.8", runtime: false}]
-  defp system("qemu_arm"), do: [{:nerves_system_qemu_arm, "~> 1.8", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.8", runtime: false}]
-  defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
+      # Dependencies for all targets except :host
+      {:nerves_time, "~> 0.2", targets: @all_targets},
+      {:nerves_pack, "~> 0.3", targets: @all_targets},
+      {:nerves_firmware_ssh, "~> 0.3", targets: @all_targets},
+      {:nerves_runtime_shell, "~> 0.1.0", targets: @all_targets},
+      {:ring_logger, "~> 0.8", targets: @all_targets},
+
+      # Dependencies for specific targets
+      {:nerves_system_rpi, "~> 1.11", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.11", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.11", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.11", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.11", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.11", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.6", runtime: false, targets: :bbb},
+      {:nerves_system_x86_64, "~> 1.11", runtime: false, targets: :x86_64}
+    ]
+  end
 end
