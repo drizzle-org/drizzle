13a46f914291cb06dcd3e22088b77244f63248f1
Jon Carstens <jjcarstens@me.com>
2020-06-07 14:38:37 -0600

    add modal component from phx.gen.live

--- drizzle_ui/lib/drizzle_ui_web.ex
+++ drizzle_ui/lib/drizzle_ui_web.ex
@@ -82,6 +82,7 @@ defmodule DrizzleUiWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import DrizzleUiWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
